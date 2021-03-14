17939c7d69b1497f643368dbe746ac021e07146f
yohantha.gunawarna <yohantha.gunawarna@progressivecontent.com>
2021-03-14 22:39:26 +0000

    added logger as a global middleware

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ const debug = require('debug')('app');
 
 const Express = require('express');
 const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const config = require('config');
 const routes = require('./routes');
 const mongoose = require('./lib/mongoose');
 const { cookieTokenHandler } = require('./lib/cookie_tokens');
+const logger = require('./lib/logger');
 
 const app = new Express();
 
@@ -15,6 +17,12 @@ const AppConfig = config.get('App');
 
 // Allow cors everywhere
 app.use(cors());
+
+// parse Cookie header
+app.use(cookieParser());
+
+// logger as a middleware
+app.use(logger());
 
 // mongo db as a middleware
 app.use(mongoose());
