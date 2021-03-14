a5c818cbf58d07879b8051525d6362b1d4ae229e
yohantha.gunawarna <yohantha.gunawarna@progressivecontent.com>
2021-03-14 22:40:49 +0000

    added logger to cooke token library

--- src/lib/cookie_tokens/cookieTokens.js
+++ src/lib/cookie_tokens/cookieTokens.js
@@ -1,3 +1,5 @@
+const debug = require('debug')('app:lib:cookie_tokens:cookieTokens');
+
 const { v4: uuidv4 } = require('uuid');
 const config = require('config');
 const CookieConfig = config.get('Cookies');
@@ -26,10 +28,17 @@ const getCookieToken = (headerCookies) => {
 
 const cookieTokenHandler = (req, res, next) => {
   const headerCookies = req.headers.cookie || null;
+  const { logger } = req.app.locals;
 
   const { cookieName, expiryDate, httpOnly, secure } = CookieConfig.GuestToken;
 
-  const cookies = factory.getCookieToken(headerCookies);
+  let cookies = [];
+  try {
+    cookies = factory.getCookieToken(headerCookies);
+  } catch (e) {
+    debug(e);
+    logger.error(e.message);
+  }
 
   if (cookies.length === 0) {
     // Cookie Token
