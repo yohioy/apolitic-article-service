158f58d442e3baf3e742b1df26a0a036f5e0f31c
yohantha.gunawarna <yohantha.gunawarna@progressivecontent.com>
2021-03-14 22:42:19 +0000

    added route middleware to check the guest id token

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,12 +2,18 @@ const { Router } = require('express');
 
 /* Controllers */
 const articlesController = require('../controllers/ArticlesController');
+const userArticleLogController = require('../controllers/UserArticleLogController');
 
 const router = Router();
 
 router.get('/articles', articlesController.getAll);
 
-router.get('/articles/:id', articlesController.getById);
+router.get(
+  '/articles/:id',
+  userArticleLogController.checkAccess,
+  userArticleLogController.createUserLog,
+  articlesController.getById
+);
 
 router.get('/health', (req, res) => {
   res.status(200).json({ healthy: true });
