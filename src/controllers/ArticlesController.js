3c24725cd20797c44348a2cf14e18c775771f60f
yohantha.gunawarna <yohantha.gunawarna@progressivecontent.com>
2021-03-14 15:07:35 +0000

    Added Articles controller

---
+++ src/controllers/ArticlesController.js
@@ -0,0 +1,38 @@
+const debug = require('debug')('app:controllers:ArticlesController');
+
+const Article = require('../modules/articles/Article');
+
+class ArticlesController {
+  async getAll(req, res) {
+    debug(`get all articles`);
+    const article = new Article();
+
+    let data;
+    try {
+      data = await article.getAll('1');
+      debug(`total articles ${data.total}`);
+      res.status(200).json(data);
+    } catch (e) {
+      debug(e);
+      res.status(400).json({ message: 'failed' });
+    }
+  }
+
+  async getById(req, res) {
+    debug(`get articles by ID`);
+    const articleId = req.params.id;
+    const article = new Article();
+
+    let data;
+    try {
+      data = await article.getById(articleId, '1');
+      debug(`article id ${articleId}`);
+      res.status(200).json(data);
+    } catch (e) {
+      debug(e);
+      res.status(400).json({ message: 'failed' });
+    }
+  }
+}
+
+module.exports = new ArticlesController();
---
+++ src/controllers/ArticlesController.spec.js
@@ -0,0 +1,117 @@
+const chai = require('chai');
+const sinon = require('sinon');
+const chaiAsPromised = require('chai-as-promised');
+const sinonChai = require('sinon-chai');
+const Article = require('../modules/articles/Article');
+
+chai.use(chaiAsPromised);
+chai.use(sinonChai);
+
+const { expect } = chai;
+
+const mockResponse = () => {
+  const res = {};
+  res.status = sinon.stub().returns(res);
+  res.json = sinon.stub().returns(res);
+  return res;
+};
+
+const mockRequest = (data) => {
+  return data;
+};
+
+describe('src/controllers/ArticlesController', () => {
+  const articlesController = require('./ArticlesController');
+
+  it('should return an Object', () => {
+    expect(articlesController).to.be.an('object');
+  });
+
+  describe('getAll()', () => {
+    it('should return status 200 with object on success', async () => {
+      const req = mockRequest({});
+      const res = mockResponse();
+      const data = [
+        {
+          id: 'a123-b175-47ba-b9d9-94f2ce9f5b14',
+          name: 'article 1',
+          author: 'author 1',
+          articleStatus: '1',
+        },
+      ];
+      const response = { total: data.length, data: data };
+
+      const stub = sinon.stub(Article.prototype, 'getAll').callsFake(() => {
+        return response;
+      });
+
+      await articlesController.getAll(req, res);
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(response);
+      expect(stub.called).to.be.equal(true);
+      stub.restore();
+    });
+
+    it('should return status 400 with object on fail', async () => {
+      const req = mockRequest({});
+      const res = mockResponse();
+
+      const response = { message: 'failed' };
+
+      const stub = sinon.stub(Article.prototype, 'getAll').throws(() => {
+        return new Error();
+      });
+
+      await articlesController.getAll(req, res);
+      expect(res.status).to.have.been.calledWith(400);
+      expect(res.json).to.have.been.calledWith(response);
+      expect(stub.called).to.be.equal(true);
+      stub.restore();
+    });
+  });
+
+  describe('getById()', () => {
+    it('should return status 200 with object on success', async () => {
+      const req = mockRequest({
+        params: { id: 'a123-b175-47ba-b9d9-94f2ce9f5b14' },
+      });
+      const res = mockResponse();
+      const data = {
+        id: 'a123-b175-47ba-b9d9-94f2ce9f5b14',
+        name: 'article 1',
+        author: 'author 1',
+        articleStatus: '1',
+      };
+
+      const response = { data: data };
+
+      const stub = sinon.stub(Article.prototype, 'getById').callsFake(() => {
+        return response;
+      });
+
+      await articlesController.getById(req, res);
+      expect(res.status).to.have.been.calledWith(200);
+      expect(res.json).to.have.been.calledWith(response);
+      expect(stub.called).to.be.equal(true);
+      stub.restore();
+    });
+
+    it('should return status 400 with object on fail', async () => {
+      const req = mockRequest({
+        params: { id: 'a123-b175-47ba-b9d9-94f2ce9f5b14' },
+      });
+      const res = mockResponse();
+      const response = { message: 'failed' };
+
+      const stub = sinon.stub(Article.prototype, 'getById').throws(() => {
+        return new Error();
+      });
+
+      await articlesController.getById(req, res);
+      expect(res.status).to.have.been.calledWith(400);
+      expect(res.json).to.have.been.calledWith(response);
+      expect(stub.called).to.be.equal(true);
+      stub.restore();
+    });
+  });
+});
