391eced5aa22440f09f1fbbbb46e5536fe3b8834
yohantha.gunawarna <yohantha.gunawarna@progressivecontent.com>
2021-03-14 19:19:26 +0000

    added controller middleware to the article routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,13 @@
 const { Router } = require('express');
 
+/* Controllers */
+const articlesController = require('../controllers/ArticlesController');
+
 const router = Router();
 
-router.get('/articles', (req, res) => {
-  res.status(200).json({});
-});
+router.get('/articles', articlesController.getAll);
 
-router.get('/articles/:id', (req, res) => {
-  res.status(200).json({});
-});
+router.get('/articles/:id', articlesController.getById);
 
 router.get('/health', (req, res) => {
   res.status(200).json({ healthy: true });
