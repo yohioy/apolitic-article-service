a333772c0f1cf0ff75d67934209c1151a9d00f8d
yohantha.gunawarna <yohantha.gunawarna@progressivecontent.com>
2021-03-15 01:33:57 +0000

    refactor

--- src/controllers/UserArticleLogController.js
+++ src/controllers/UserArticleLogController.js
@@ -1,17 +1,28 @@
 const debug = require('debug')('app:controllers:UserAccessController');
 
+const config = require('config');
+
 const UserArticleLog = require('../modules/user_article_logs/UserArticleLog');
 
 class UserArticleLogController {
-
   async createUserLog(req, res, next) {
     debug(`create user log`);
 
-    const { logger, guestToken } = req.app.locals;
+    const { logger, cookieTokens } = req.app.locals;
     const articleId = req.params.id;
+
+    const CookieConfig = config.get('Cookies');
+    const { cookieName } = CookieConfig.GuestToken;
+
     const userArticleLog = new UserArticleLog();
 
-    const accessLog = await userArticleLog.getByGuestId(guestToken);
+    const validCookie = cookieTokens.filter((item) => {
+      return item.name === cookieName;
+    });
+    const guestId = validCookie[0].value;
+
+    const accessLog = await userArticleLog.getByGuestId(guestId);
+    debug(accessLog);
 
     let newViewedArticles = [];
     if (accessLog) {
@@ -22,7 +33,7 @@ class UserArticleLogController {
     }
 
     const newData = {
-      guestId: guestToken,
+      guestId: guestId,
       viewedArticles: newViewedArticles,
     };
 
@@ -32,7 +43,49 @@ class UserArticleLogController {
       next();
     } catch (e) {
       debug(e);
+      logger.error(e.message);
       res.status(400).json({ message: 'failed creating log' });
+    }
+  }
+
+  async checkAccess(req, res, next) {
+    debug(`check user access logs`);
+
+    const { logger, cookieTokens } = req.app.locals;
+    const CookieConfig = config.get('Cookies');
+    const { cookieName } = CookieConfig.GuestToken;
+
+    const userArticleLog = new UserArticleLog();
+
+    if (cookieTokens.length === 0) {
+      debug(`Cannot find token`);
+      logger.error(`Cannot find token`);
+      return res.status(400).json({ message: 'cannot find token' });
+    }
+
+    const validCookie = cookieTokens.filter((item) => {
+      return item.name === cookieName;
+    });
+    const guestId = validCookie[0].value;
+
+    let accessLog;
+    try {
+      accessLog = await userArticleLog.getByGuestId(guestId);
+
+      if (accessLog) {
+        if (accessLog.viewedArticles.length >= 3) {
+          debug(`cannot view more articles`);
+          res.status(400).json({ message: 'cannot view more articles' });
+        } else {
+          next();
+        }
+      } else {
+        next();
+      }
+    } catch (e) {
+      debug(e);
+      logger.error(e.message);
+      res.status(400).json({ message: 'failed' });
     }
   }
 }
