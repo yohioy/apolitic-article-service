797af2394f1bfca6ad8cf237406001f5d0e9c149
yohantha.gunawarna <yohantha.gunawarna@progressivecontent.com>
2021-03-15 01:33:25 +0000

    stub logger

--- src/lib/mongoose/mongoose.js
+++ src/lib/mongoose/mongoose.js
@@ -5,11 +5,13 @@ const { uri, options } = require('./config');
 
 const connection = () => {
   return async (req, res, next) => {
+    const { logger } = req.app.locals;
     try {
       await mongoose.connect(uri, options);
       next();
     } catch (e) {
       debug('Error', e);
+      logger.error(e.message);
       res.status(500).json({ message: 'Database connection error' });
     }
   };
